3121d026416f8b68fad896cf86977dea52563870
aki158 <hideponpon1931@gmail.com>
2021-01-07 18:00:31 +0900

    37 HTMLヘッダー(文字フォントの変更)

--- nuxt-view/nuxt.config.js
+++ nuxt-view/nuxt.config.js
@@ -1,14 +1,15 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-view',
+    title: 'Hello Nuxt.js!',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap'}
     ]
   },
 
