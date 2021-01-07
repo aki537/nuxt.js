0097c5e4c6885af84647e594eca08e08d4cc94c5
aki158 <hideponpon1931@gmail.com>
2021-01-07 17:13:44 +0900

    30 動的なルーティング

---
+++ nuxt-routing/pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>{{ message }}</h1>
+    <!-- this.$route.params.idでURLに売った/users/1の1が取得できる -->
+    <p>id: {{ this.$route.params.id }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: '/users/_id.vueを表示中'
+    }
+  }
+}
+</script>
