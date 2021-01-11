b64acc18fd3541d245239f5313344471f77b2c68
aki158 <hideponpon1931@gmail.com>
2021-01-11 15:33:51 +0900

    83 Todosコンポーネントの作成

---
+++ nuxt-todo/pages/todos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  // createのタイミングでinitが呼び込まれfirebaseの初期化が行われる
+  created: function() {
+    this.$store.dispatch('todos/init')
+  }
+}
+</script>
