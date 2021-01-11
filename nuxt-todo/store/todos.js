0d6a18540669697d79c85f852546847f96f22b0c
aki158 <hideponpon1931@gmail.com>
2021-01-11 15:27:25 +0900

    82 todos.jsの説明

--- nuxt-todo/store/todos.js
+++ nuxt-todo/store/todos.js
@@ -4,14 +4,17 @@ import { firestoreAction } from 'vuexfire'
 const db = firebase.firestore()
 const todosRef =db.collection('todos')
 
+// todo一覧を管理するtodosを配列で定義する
 export const state = () => ({
   todos:[]
 })
 
 export const actions = {
-  init: firestoreAction(({ bindFirestoreRef}) => {
+  // todosの初期化
+  init: firestoreAction(({ bindFirestoreRef }) => {
     bindFirestoreRef('todos', todosRef)
   }),
+  // todosの追加
   add: firestoreAction((context, name) => {
     if(name, trim()){
       todosRef.add({
@@ -21,9 +24,11 @@ export const actions = {
       })
     }
   }),
+  // todosの削除
   remove: firestoreAction((context, id) => {
     todosRef.doc(id).delete()
   }),
+  // チェックボックス操作時の処理
   toggle: firestoreAction((context, todo) =>{
     todosRef.doc(todo.id).update({
       done: !todo.done
