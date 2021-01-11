f2164e85dffda68fa70a321237191d5af431e1bf
aki158 <hideponpon1931@gmail.com>
2021-01-11 12:08:18 +0900

    カウントアップ実装

---
+++ nuxt-vuex-counter/store/counter.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+  count: 0
+})
+
+export const mutations = {
+  countUp: function(state) {
+    state.count += 1
+  }
+}
+
+export const actions = {
+  updateMessageAction(context, payload) {
+    context.commit('updateMessage', payload)
+  }
+}
